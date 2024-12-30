2264db1794f17bc37db4ae6db89d89723397e434
Shakoor Ahmed <126576979+shakoor7882@users.noreply.github.com>
2024-12-30 09:01:22 +0300

    Update delete-script.sh

--- delete-script.sh
+++ delete-script.sh
@@ -3,4 +3,4 @@
 find . -type d -name ".ter*" -exec rm -rf {} +;
 find . -type f -name ".ter*" -exec rm -rf {} +;
 #find . -type d -name "ter*" -exec rm -rf {} +;
-#find . -type f -name "ter*" -exec rm -rf {} +;
+#find . -type f -name "ter*" -exec rm -rf {} ++;
